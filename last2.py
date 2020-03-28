ee0a33c948e2773807518665f77acdf432b78c4b
naveed3923 <61916273+naveed3923@users.noreply.github.com>
2020-03-28 14:04:25 +0530

    Update last2.py

--- last2.py
+++ last2.py
@@ -1,3 +1,8 @@
+#Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars 
+#of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+
 def last2(str):
     if len(str)<2:
         return 0

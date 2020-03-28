b7546d218d9cf1d600854697f72917daa826d18e
naveed3923 <61916273+naveed3923@users.noreply.github.com>
2020-03-28 14:01:20 +0530

    Update string_match.py

--- string_match.py
+++ string_match.py
@@ -1,3 +1,7 @@
+#Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. 
+#So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+
+
 def string_match(a, b):
     min_len=min(len(a),len(b))
     count=0
